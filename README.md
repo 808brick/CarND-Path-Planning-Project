db878936b999edf6fcf59180097c2354966b6dff
Raymond Andrade <raymond808state1@gmail.com>
2020-03-07 19:26:50 -1000

    Implemented spline path generation, cost functions for driving on highway.

--- README.md
+++ README.md
@@ -1,21 +1,64 @@
 # CarND-Path-Planning-Project
-Self-Driving Car Engineer Nanodegree Program
-   
+This is the path planning project associated with the Udacity Self Driving Car Engineer Nanodegree program.
+
+### Goals
+The goal of this project is to implement Path Planning and Behavior Planning algorithms to safely navigate around a highway in a virtual simulation. One of the challenges of this simulation is that it includes other cars on the highway, which drive at varying speeds and change lanes. There is also a imposed speed limit of 22.35 meters/sec (50 MPH) which is not to be exceeded, and any jerk from the car suddenly accelerating should be minimized (should not exceed acceleration over 10 meters/sec^2, or jerk over 10 meters/sec^3). A map of the highway is provided in the from of a csv file which contains the x and y position of the highway which it wants to navigate to. It also converts these coordinates to the s and d coordinate system for classifying position on a road. The highway is 6946 meters (~4.32 miles) long.
+
+### Highway Map
+The map of the highway is a csv file in data/highway_map.txt. Each waypoint in the list contains  [x,y,s,dx,dy] values. x and y are the waypoint's map coordinate position, the s value is the distance along the road to get to that waypoint in meters, the dx and dy values define the unit normal vector pointing outward of the highway loop.
+
+The highway's waypoints loop around so the frenet s value, distance along the road, goes from 0 to 6945.554.
+
+## Approach
+
+| Input | Description  |
+|:-:|-|
+| x,y  | Car's current x,y position (relative to map)  |
+| yaw  | Car's current heading (relative to map)  |
+| speed  | Car's current speed (in MPH)  |
+| s,d  | Car's current s,d position (relative to road)  |
+| sensor_fusion  | 2D vector of nearby cars (x,y,Vx,Vy,s,d)  |
+
+| Output | Description  |
+|:-:|-|
+| next_x_vals  | List of x coordinates to pass the simulator for the car to traverse  |
+| next_y_vals  | List of y coordinates to pass the simulator for the car to traverse  |
+
+
+### Behavior Planning
+There are a couple things to make sure our autonomous car considers when driving on the highway. For instance, we want to ensure our car is following the speed limit, is driving safely, stays in its own lane (unless changing lanes), and change lanes if the car in front is driving much below the speed limit. The value for each of these attributes are different, such that safety and legality is of most importance. We can rate the value of each action at any given time using the input data and putting it through a cost function, to deem what action generates the best outcome for the given situation.  
+
+##### Cost Functions
+| Attribute | Cost Determination  |
+|:-:|-|
+| Speed  | Cost increases if going too slow, or too fast (relative to speed limit), also depends on speed of car in front |
+| Staying in Lane | Cost increases if we are not centered in our lane |
+| Changing Lane | Cost decreases if car in front is going slow, goal distance is far, and lane to merge in is clear. Broken up into individual left and right turn cost |
+|  | |
+|  | |
+
+
+### Trajectory Generation
+
+##### Hybrid A*
+
+##### Fernet Coordinates
+
+##### Jerk Minimization
+
+##### Polynomial Trajectory Generation
+
+
+
+## Other Background Information:
+
 ### Simulator.
-You can download the Term3 Simulator which contains the Path Planning Project from the [releases tab (https://github.com/udacity/self-driving-car-sim/releases/tag/T3_v1.2).  
+You can download the Term3 Simulator which contains the Path Planning Project from the [releases tab](https://github.com/udacity/self-driving-car-sim/releases/tag/T3_v1.2).  
 
 To run the simulator on Mac/Linux, first make the binary file executable with the following command:
 ```shell
 sudo chmod u+x {simulator_file_name}
 ```
-
-### Goals
-In this project your goal is to safely navigate around a virtual highway with other traffic that is driving +-10 MPH of the 50 MPH speed limit. You will be provided the car's localization and sensor fusion data, there is also a sparse map list of waypoints around the highway. The car should try to go as close as possible to the 50 MPH speed limit, which means passing slower traffic when possible, note that other cars will try to change lanes too. The car should avoid hitting other cars at all cost as well as driving inside of the marked road lanes at all times, unless going from one lane to another. The car should be able to make one complete loop around the 6946m highway. Since the car is trying to go 50 MPH, it should take a little over 5 minutes to complete 1 loop. Also the car should not experience total acceleration over 10 m/s^2 and jerk that is greater than 10 m/s^3.
-
-#### The map of the highway is in data/highway_map.txt
-Each waypoint in the list contains  [x,y,s,dx,dy] values. x and y are the waypoint's map coordinate position, the s value is the distance along the road to get to that waypoint in meters, the dx and dy values define the unit normal vector pointing outward of the highway loop.
-
-The highway's waypoints loop around so the frenet s value, distance along the road, goes from 0 to 6945.554.
 
 ## Basic Build Instructions
 
@@ -23,51 +66,6 @@ The highway's waypoints loop around so the frenet s value, distance along the ro
 2. Make a build directory: `mkdir build && cd build`
 3. Compile: `cmake .. && make`
 4. Run it: `./path_planning`.
-
-Here is the data provided from the Simulator to the C++ Program
-
-#### Main car's localization Data (No Noise)
-
-["x"] The car's x position in map coordinates
-
-["y"] The car's y position in map coordinates
-
-["s"] The car's s position in frenet coordinates
-
-["d"] The car's d position in frenet coordinates
-
-["yaw"] The car's yaw angle in the map
-
-["speed"] The car's speed in MPH
-
-#### Previous path data given to the Planner
-
-//Note: Return the previous list but with processed points removed, can be a nice tool to show how far along
-the path has processed since last time. 
-
-["previous_path_x"] The previous list of x points previously given to the simulator
-
-["previous_path_y"] The previous list of y points previously given to the simulator
-
-#### Previous path's end s and d values 
-
-["end_path_s"] The previous list's last point's frenet s value
-
-["end_path_d"] The previous list's last point's frenet d value
-
-#### Sensor Fusion Data, a list of all other car's attributes on the same side of the road. (No Noise)
-
-["sensor_fusion"] A 2d vector of cars and then that car's [car's unique ID, car's x position in map coordinates, car's y position in map coordinates, car's x velocity in m/s, car's y velocity in m/s, car's s position in frenet coordinates, car's d position in frenet coordinates. 
-
-## Details
-
-1. The car uses a perfect controller and will visit every (x,y) point it recieves in the list every .02 seconds. The units for the (x,y) points are in meters and the spacing of the points determines the speed of the car. The vector going from a point to the next point in the list dictates the angle of the car. Acceleration both in the tangential and normal directions is measured along with the jerk, the rate of change of total Acceleration. The (x,y) point paths that the planner recieves should not have a total acceleration that goes over 10 m/s^2, also the jerk should not go over 50 m/s^3. (NOTE: As this is BETA, these requirements might change. Also currently jerk is over a .02 second interval, it would probably be better to average total acceleration over 1 second and measure jerk from that.
-
-2. There will be some latency between the simulator running and the path planner returning a path, with optimized code usually its not very long maybe just 1-3 time steps. During this delay the simulator will continue using points that it was last given, because of this its a good idea to store the last points you have used so you can have a smooth transition. previous_path_x, and previous_path_y can be helpful for this transition since they show the last points given to the simulator controller with the processed points already removed. You would either return a path that extends this previous path or make sure to create a new path that has a smooth transition with this last path.
-
-## Tips
-
-A really helpful resource for doing this project and creating smooth trajectories was using http://kluge.in-chemnitz.de/opensource/spline/, the spline function is in a single hearder file is really easy to use.
 
 ---
 
@@ -87,59 +85,7 @@ A really helpful resource for doing this project and creating smooth trajectorie
   * Run either `install-mac.sh` or `install-ubuntu.sh`.
   * If you install from source, checkout to commit `e94b6e1`, i.e.
     ```
-    git clone https://github.com/uWebSockets/uWebSockets 
+    git clone https://github.com/uWebSockets/uWebSockets
     cd uWebSockets
     git checkout e94b6e1
     ```
-
-## Editor Settings
-
-We've purposefully kept editor configuration files out of this repo in order to
-keep it as simple and environment agnostic as possible. However, we recommend
-using the following settings:
-
-* indent using spaces
-* set tab width to 2 spaces (keeps the matrices in source code aligned)
-
-## Code Style
-
-Please (do your best to) stick to [Google's C++ style guide](https://google.github.io/styleguide/cppguide.html).
-
-## Project Instructions and Rubric
-
-Note: regardless of the changes you make, your project must be buildable using
-cmake and make!
-
-
-## Call for IDE Profiles Pull Requests
-
-Help your fellow students!
-
-We decided to create Makefiles with cmake to keep this project as platform
-agnostic as possible. Similarly, we omitted IDE profiles in order to ensure
-that students don't feel pressured to use one IDE or another.
-
-However! I'd love to help people get up and running with their IDEs of choice.
-If you've created a profile for an IDE that you think other students would
-appreciate, we'd love to have you add the requisite profile files and
-instructions to ide_profiles/. For example if you wanted to add a VS Code
-profile, you'd add:
-
-* /ide_profiles/vscode/.vscode
-* /ide_profiles/vscode/README.md
-
-The README should explain what the profile does, how to take advantage of it,
-and how to install it.
-
-Frankly, I've never been involved in a project with multiple IDE profiles
-before. I believe the best way to handle this would be to keep them out of the
-repo root to avoid clutter. My expectation is that most profiles will include
-instructions to copy files to a new location to get picked up by the IDE, but
-that's just a guess.
-
-One last note here: regardless of the IDE used, every submitted project must
-still be compilable with cmake and make./
-
-## How to write a README
-A well written README file can enhance your project and portfolio.  Develop your abilities to create professional README files by completing [this free course](https://www.udacity.com/course/writing-readmes--ud777).
-
--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,132 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
 using std::string;
 using std::vector;
+
+
+////////////////////
+//  Helper Functions
+////////////////////
+
+double mph_to_m_s(double mph_speed){
+  return mph_speed * 0.44704;
+}
+
+double m_s_to_mph(double m_s_speed){
+  return m_s_speed / 0.44704;
+}
+
+
+
+////////////////////
+//  Variables
+////////////////////
+
+int max_lane_num = 2; // On this highway, there are lanes 3 lanes, [0,1,2]
+double lane_width = 4.0; // Width of highway lane in meters
+
+double speed_limit_mph = 50; // Speed limit in MPH
+double speed_limit = mph_to_m_s(speed_limit_mph); // Speed limit in m/s
+double speed_limit_buffer = mph_to_m_s(1); // Buffer to ensure we do not accidentally exceed speed limit
+double desired_speed = speed_limit - speed_limit_buffer;
+double max_acceleration = 0.2; // Do not accelerate too fast to try minimize jerk
+
+double safe_lane_change_dist = 30; // Distance away in meters cars in other lane should be to safely change lane
+
+double max_cost = 1;
+double slow_down_default_cost = 0.5;
+double speed_up_default_cost = 0.7;
+double stay_in_lane_default_cost = 0.4;
+double turning_min_cost = 0.25; // Default cost of turning, prioritize staying in lane unless there is a slow car in front
+
+
+////////////////////
+//  Cost Functions
+////////////////////
+
+double stay_in_lane_cost(double car_front_dist){
+  double cost = max_cost;
+  if (car_front_dist > safe_lane_change_dist*1.5){
+    cost *= 0.2;
+  }
+  else { // Increase cost as vehicle in front gets closer
+    double slope = 1.0 * (0.8 - 0) / (safe_lane_change*1.5 - 0);
+    double mapped_value = 0 + slope * (car_front_dist);
+    cost *= 0.2 + (1 - mapped_value);
+  }
+  return cost;
+}
+
+double slow_down_cost(double car_front_dist){
+  double cost = slow_down_default_cost;
+
+  if (current_speed > speed_limit || car_front_dist < 5){
+    cost = 0;
+  }
+
+  else if (car_front_dist < 10){
+    cost *= 0.5
+  }
+  else if (current_speed > desired_speed){
+    cost *= 0.3;
+  }
+  return cost;
+}
+
+double speed_up_cost(double car_front_dist){
+  double cost = speed_up_default_cost;
+
+  if (current_speed < desired_speed && car_front_dist > safe_lane_change_dist){
+    cost = speed_up_default_cost * 0.3;
+  }
+  return cost;
+
+}
+
+
+double turn_left_cost(bool car_on_left, double car_left_front_dist){
+  double cost = max_cost;
+
+  if (current_lane != 0 || car_on_left == false){
+
+    cost -= car_left_front_dist / (2 * safe_lane_change_dist);
+    // cost *= safe_lane_change_dist /2 / car_left_front_dist);
+
+    // Ensure cost never goes below turning_min_cost
+    if (cost < turning_min_cost){
+      cost = turning_min_cost;
+    }
+
+  }
+
+  return cost;
+
+
+}
+
+double turn_right_cost(bool car_on_right, double car_right_front_dist){
+  double cost = max_cost;
+
+  if (current_lane != max_lane_num || car_on_right == false){
+    cost -= car_right_front_dist / (2 * safe_lane_change_dist);
+
+    // Ensure cost never goes below turning_min_cost
+    if (cost < turning_min_cost){
+      cost = turning_min_cost;
+    }
+  }
+
+  return cost;
+}
+
+////////////////////
+//  Main
+////////////////////
 
 int main() {
   uWS::Hub h;
@@ -35,9 +156,9 @@ int main() {
     std::istringstream iss(line);
     double x;
     double y;
-    float s;
-    float d_x;
-    float d_y;
+    double s;
+    double d_x;
+    double d_y;
     iss >> x;
     iss >> y;
     iss >> s;
@@ -63,12 +184,12 @@ int main() {
 
       if (s != "") {
         auto j = json::parse(s);
-        
+
         string event = j[0].get<string>();
-        
+
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
+
           // Main car's localization Data
           double car_x = j[1]["x"];
           double car_y = j[1]["y"];
@@ -80,11 +201,11 @@ int main() {
           // Previous path data given to the Planner
           auto previous_path_x = j[1]["previous_path_x"];
           auto previous_path_y = j[1]["previous_path_y"];
-          // Previous path's end s and d values 
+          // Previous path's end s and d values
           double end_path_s = j[1]["end_path_s"];
           double end_path_d = j[1]["end_path_d"];
 
-          // Sensor Fusion Data, a list of all other cars on the same side 
+          // Sensor Fusion Data, a list of all other cars on the same side
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
@@ -98,6 +219,207 @@ int main() {
            *   sequentially every .02 seconds
            */
 
+           ////////////////////
+           //  Variables
+           ////////////////////
+
+           double delta_t = 0.02;
+
+           tk::spline spline_path;
+           vector<double> x_path_points, y_path_points;
+
+           int decision = 0;
+           // decision = 0 = Stay in same lane at max velocity
+           // decision = 1 = Slow Down
+           // decision = 2 = Turn Left
+           // decision = 3 = Turn right
+
+           int current_lane = car_d / lane_width; // 0 if in left lane, 1 if in middle lane, 2
+           int desired_lane = current_lane; // Will change if decide to change lanes
+
+           double speed_diff = 0;
+
+           vector<double> cost_vector;
+           double car_front_dist = 100;
+           double car_left_front_dist = 100;
+           double car_right_front_dist = 100;
+           bool car_on_left = false;
+           bool car_on_right = false;
+
+
+
+
+           ////////////////////
+           //  Make Decision
+           ////////////////////
+
+           for (int i=0; i < sensor_fusion.size(); i++){ // Loop through each car in the simulation
+             auto car_i_data = sensor_fusion[i];
+             double car_i_s = car_i_date[5];
+             double car_i_d = car_i_data[6];
+             double car_i_lane_num = car_i_s / lane_width;
+             double dist_from_car_i = car_i_s - car_s;
+             bool car_i_in_front = false;
+
+             if (dist_from_car_i > 0){ // Car is in front of us
+               car_i_in_front = true;
+             }
+
+             if (car_i_lane_num == current_lane && car_i_in_front && car_front_dist > dist_from_car_i){ // Check if car is in same lane, in front
+               car_front_dist = dist_from_car_i;
+             }
+
+             if (car_i_lane_num == current_lane - 1){
+               if (abs(dist_from_car_i) < safe_lane_change_dist){
+                 car_on_left = true;
+               }
+               if (car_i_in_front && dist_from_car_i < car_left_front_dist) {
+                 car_left_front_dist = dist_from_car_i;
+               }
+             }
+
+             if (car_i_lane_num == current_lane + 1){
+               if (abs(dist_from_car_i) < safe_lane_change_dist){
+                 car_on_right = true;
+               }
+               if (car_i_in_front && dist_from_car_i < car_right_front_dist) {
+                 car_right_front_dist = dist_from_car_i;
+               }
+             }
+
+           }
+
+           cost_vector.push_back(stay_in_lane_cost(car_front_dist));
+           cost_vector.push_back(slow_down_cost(car_front_dist));
+           cost_vector.push_back(turn_left_cost(car_on_left, car_left_front_dist));
+           cost_vector.push_back(turn_right_cost(car_on_right, car_right_front_dist));
+
+           std::cout << std::endl;
+           std::cout << "Car Front Dist: " << car_front_dist << std::endl;
+           std::cout << "Car Front Left Dist: " << car_left_front_dist << std::endl;
+           std::cout << "Car On Left: " << car_on_left << std::endl;
+           std::cout << "Car On Right: " << car_on_right << std::endl;
+           std::cout << "Cost Function: " << std::endl;
+           std::cout << cost_vector[0] << std::endl;
+           std::cout << cost_vector[1] << std::endl;
+           std::cout << cost_vector[2] << std::endl;
+           std::cout << cost_vector[3] << std::endl;
+           std::cout << std::endl;
+
+           if (decision == 0) { // Continue straight in lane
+             desired_lane = current_lane;
+             if (car_speed < desired_speed){ // Increase speed if not going at desired speed
+               speed_diff += max_acceleration;
+             }
+           }
+
+           else if (decision == 1) { // Slow Down
+              speed_diff -= max_acceleration;
+           }
+
+           else if (decision == 2) { // Turn Left
+              desired_lane -= 1;
+           }
+
+           else if (decision == 2) { // Turn Right
+              desired_lane += 1;
+           }
+
+
+           ////////////////////
+           //  Generate Path Based Off Decision
+           ////////////////////
+
+           vector<double> points_x, points_y;
+
+           double ref_x = car_x;
+           double ref_y = car_y;
+           double ref_yaw = car_yaw;
+
+           int prev_size = previous_path_x.size();
+
+           if (prev_size < 2){
+             double prev_car_x = car_x - cos(car_yaw);
+             double prev_car_y = car_y - sin(car_yaw);
+
+             points_x.push_back(prev_car_x);
+             points_y.push_back(prev_car_y);
+
+             points_x.push_back(car_x);
+             points_y.push_back(car_y);
+
+           }
+
+           else{
+             ref_x = previous_path_x[prev_car_x - 1];
+             ref_y = previous_path_y[prev_car_y - 1];
+
+             double ref_x_prev = previous_path_x[prev_size-2];
+             double ref_y_prev = previous_path_y[prev_size-2];
+             ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+             points_x.push_back(ref_x_prev);
+             points_y.push_back(ref_y_prev);
+
+             points_x.push_back(ref_x);
+             points_y.push_back(ref_y);
+           }
+
+           vector<double> next_wp0 = getXY(car_s+30, ((desired_lane + 0.5)*lane_width), map_waypoints_s, map_waypoints_x, map_waypoints_y );
+           vector<double> next_wp1 = getXY(car_s+45, ((desired_lane + 0.5)*lane_width), map_waypoints_s, map_waypoints_x, map_waypoints_y );
+           vector<double> next_wp2 = getXY(car_s+60, ((desired_lane + 0.5)*lane_width), map_waypoints_s, map_waypoints_x, map_waypoints_y );
+
+           points_x.push_back(next_wp0[0]);
+           points_y.push_back(next_wp0[1]);
+           points_x.push_back(next_wp1[0]);
+           points_y.push_back(next_wp1[1]);
+           points_x.push_back(next_wp2[0]);
+           points_y.push_back(next_wp2[1]);
+
+           for (int i = 0; i < points_x.size(); i++){
+             // Localize car in it's own local coordinate system
+             double shift_x = points_x[i]-ref_x;
+             double shift_y = points_y[i]-ref_y;
+
+             points_x[i] = shift_x * cos(0-ref_yaw)- shift_y*sin(0-ref_yaw);
+             points_y[i] = shift_x * sin(0-ref_yaw)- shift_y*cos(0-ref_yaw);
+
+           }
+
+           spline_path.set_points(points_x, points_y);
+
+           // Any points generated previously, add them to the new list of points
+           for(int i = 0; i < prev_size; i++){
+             next_x_vals.push_back(previous_path_x[i]);
+             next_y_vals.push_back(previous_path_y[i]);
+           }
+
+           double target_x = 30.0;
+           double target_y = spline_path(target_x);
+           double target_dist = sqrt(target_x * target_x + target_y * target_y);
+
+           double x_add_on = 0;
+
+           double ref_velocity = car_speed + speed_diff;
+
+           for(int i = 1; i<=50-prev_size; i++){
+             double n = target_dist/(delta_t*ref_velocity);
+             double x_point = x_add_on+(target_x)/n;
+             double y_point = spline_path(x_point);
+
+             x_add_on = x_point;
+
+             double x_ref = x_point;
+             double y_ref = y_point;
+
+             // Switch back to the map coordinate system
+             x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw)) + ref_x;
+             y_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw)) + ref_x;
+
+             next_x_vals.push_back(x_points);
+             next_y_vals.push_back(y_points);
+
+           }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
@@ -131,6 +453,6 @@ int main() {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
